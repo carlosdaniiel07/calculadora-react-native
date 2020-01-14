4a3979c963223002f2bfea1c7f3e8d2efcf2042d
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2020-01-13 22:38:42 -0300

    Montando estrutura básica do layout da calculadora

--- App.js
+++ App.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
+import Botao from './src/components/Botao'
+
+const Style = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  display: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  buttons: {
+    flex: 1,
+    flexDirection: "row"
+  }
+})
 
 export default class App extends Component {
+  onClick(label) {
+    console.log('Label => ' + label);
+  }
+
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
+      <View style={Style.container}>
+        <View style={Style.display}>
+
+        </View>
+
+        <View style={Style.buttons}>
+          <Botao label='7' onPress={this.onClick} />
+          <Botao label='8' onPress={this.onClick} />
+          <Botao label='9' onPress={this.onClick} />
+          <Botao label='AC' onPress={this.onClick} />
+        </View>
+
+        <View style={Style.buttons}>
+          <Botao label='4' onPress={this.onClick} />
+          <Botao label='5' onPress={this.onClick} />
+          <Botao label='6' onPress={this.onClick} />
+          <Botao label='x' onPress={this.onClick} />
+        </View>
+
+        <View style={Style.buttons}>
+          <Botao label='1' onPress={this.onClick} />
+          <Botao label='2' onPress={this.onClick} />
+          <Botao label='3' onPress={this.onClick} />
+          <Botao label='-' onPress={this.onClick} />
+        </View>
+
+        <View style={Style.buttons}>
+          <Botao label='.' onPress={this.onClick} />
+          <Botao label='0' onPress={this.onClick} />
+          <Botao label='=' onPress={this.onClick} />
+          <Botao label='+' onPress={this.onClick} />
+        </View>
       </View>
     )
   }
---
+++ src/components/Botao.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { Text, View, TouchableHighlight, StyleSheet, Dimensions } from 'react-native'
+
+const Style = StyleSheet.create({
+    button: {
+        fontSize: 30,
+        height: Dimensions.get('window').height / 4,
+        width: Dimensions.get('window').width / 4,
+        padding: 20,
+        backgroundColor: '#454545',
+        textAlign: "center",
+        justifyContent: "center",
+        borderWidth: 1,
+        borderColor: '#454545',
+        color: '#fff'
+    }
+})
+
+export default (props) => {
+    return (
+        <TouchableHighlight onPress={() => props.onPress(props.label)}>
+            <Text style={Style.button}>{props.label}</Text>
+        </TouchableHighlight>
+    )
+}
